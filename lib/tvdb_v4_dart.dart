e5e827de159132f6346e004aad862d603305f0ec
Phillipe Ramalho <ramalhophillipe@gmail.com>
2025-09-02 04:38:33 -0300

    chore: bump dependencies to latest versions available

--- lib/tvdb_v4_dart.dart
+++ lib/tvdb_v4_dart.dart
@@ -1,8 +1,3 @@
-/// A Dart client for TheTVDB API v4.
-///
-/// This is a direct translation of the official Python client.
-library tvdb_v4_dart;
-
 export 'src/auth.dart';
 export 'src/request.dart';
 export 'src/url.dart';
